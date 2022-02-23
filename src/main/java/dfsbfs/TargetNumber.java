871bbe8b359685e009f0b1d0d8c6b838c966eb14
iokim <iokim1220@gmail.com>
2022-02-23 14:47:06 +0900

    # 깊이/너비 우선 탐색(DFS/BFS) - LEVEL 2 - 타겟 넘버

--- src/main/java/dfsbfs/TargetNumber.java
+++ src/main/java/dfsbfs/TargetNumber.java
@@ -4,6 +4,21 @@ public class TargetNumber {
 
     public int solution(int[] numbers, int target) {
         int answer = 0;
+
+        answer = recursiveSum(numbers, target,0, 0);
+
         return answer;
+    }
+
+    public int recursiveSum(int[] numbers, int target, int sum, int index) {
+        if(index == numbers.length) {
+            if(sum == target){
+                return 1;
+            }
+            return 0;
+        }
+        // sum 값에서 +,- 두 갈래로 나누어서 계산하기..
+        return recursiveSum(numbers, target, sum + numbers[index], index+1)
+                + recursiveSum(numbers, target, sum - numbers[index], index+1);
     }
 }
