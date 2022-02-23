5c93976047d790bfcd8fbec4bbec93ca6f75d2d9
iokim <iokim1220@gmail.com>
2022-02-23 21:22:59 +0900

    no message

---
+++ src/main/java/dfsbfs/WordConversion.java
@@ -0,0 +1,11 @@
+package dfsbfs;
+
+public class WordConversion {
+
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        return answer;
+    }
+
+
+}
---
+++ src/test/java/dfsbfs/WordConversionTest.java
@@ -0,0 +1,32 @@
+package dfsbfs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordConversionTest {
+
+    WordConversion wordConversion = new WordConversion();
+
+    @Test
+    void solution1() {
+        String begin = "hit";
+        String target = "cog";
+        String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
+
+        int answer = wordConversion.solution(begin,target,words);
+
+        assertEquals(answer, 4);
+    }
+
+    @Test
+    void solution2() {
+        String begin = "hit";
+        String target = "cog";
+        String[] words = {"hot", "dot", "dog", "lot", "log"};
+
+        int answer = wordConversion.solution(begin,target,words);
+
+        assertEquals(answer, 0);
+    }
+}
